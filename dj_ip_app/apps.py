12bfe96f1c40cd0ada28c8afd7dddb09226365ec
alibekN54 <alibekandanuarbek@gmail.com>
2022-05-28 23:13:10 +0600

    initial commit

---
+++ dj_ip_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjIpAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dj_ip_app'
---
+++ dj_ip_app/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from ipware import get_client_ip
+
+from ip2geotools.databases.noncommercial import DbIpCity
+
+# Create your views here.
+
+
+def index(request):
+    client_ip, is_routable = get_client_ip(request)
+    response = DbIpCity.get('5.251.199.84', api_key='free')
+
+    if client_ip is None:
+        return HttpResponse("Unable to get the client's IP address")
+    else:
+        return HttpResponse(f'<h1>IP: {client_ip} {is_routable} city : {response.city} smthng : {response.country}</h1>')
+    if is_routable:
+        return HttpResponse("The client's IP address is publicly routable on the Internet")
+    else:
+        return HttpResponse("The client's IP address is private")
+
+
