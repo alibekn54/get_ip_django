28db3f2edc6f44aa5dc732d44edbd620a562173e
alibekN54 <alibekandanuarbek@gmail.com>
2022-05-31 20:34:52 +0600

    qwqwdqwd

--- dj_ip/settings.py
+++ dj_ip/settings.py
@@ -121,5 +121,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
