647ed54eb10c11c97c01bdc10109a2830c26f7d9
alibekN54 <alibekandanuarbek@gmail.com>
2022-06-04 13:24:55 +0600

    testtttt

--- timezone.py
+++
@@ -1,7 +0,0 @@
-from timezonefinder import TimezoneFinder
-
-tf = TimezoneFinder()
-tz = tf.timezone_at(lng=52.5061, lat=13.358)  # 'Europe/Berlin'
-ee = tf.timezone_at(lng=1.0, lat=50.5)  # 'Etc/GMT'
-
-print(ee)
